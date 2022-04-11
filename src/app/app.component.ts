b3a06a53fc75ddb78c7bfbfee799c0aaa1e36ff8
katherine villaparedes <86531447+katherinevilla@users.noreply.github.com>
2022-04-11 08:44:12 -0400

    adicionando controlvalueaccessor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'a11y-p1';
-  public yesNoAnswer = 'no';
+  //public yesNoAnswer = 'no';
+  public form: FormGroup = null;
 
+
+  constructor(private formBuilder:FormBuilder){
+    this.form = formBuilder.group({
+      yesNoAnswer :['no']
+    })
+  }
+
+  public submit(): void{
+    console.log(this.form.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
 YesNoButtonGroupModule,
+ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
